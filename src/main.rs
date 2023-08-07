27ec7807446308ce4198d90dc19d33f8fc5a6a1f
felixweiss1999 <felixweiss1999@gmail.com>
2023-08-07 23:58:28 +0200

    last commit for the day

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ fn main() {
     println!("Guess the number!");
     println!("Please input your guess!");
     let mut guess = String::new();
-    io::stdin()
+    let bytes: usize = io::stdin()
         .read_line(&mut guess)
             .expect("Failed to read line");
-    println!("You guessed: {guess}");
+    println!("You guessed: {}, and entered {bytes} bytes.", guess);
 }
