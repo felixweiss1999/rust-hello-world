a41059422678d23f8bb0cf9ac7e76f566a5b354b
felixweiss1999 <felixweiss1999@gmail.com>
2023-08-08 21:41:00 +0200

    implemented loop and variable checks, error check

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
 use std::io;
-
+use rand::Rng;
+use std::cmp::Ordering;
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess!");
-    let i: i32 = 23;
-    let mut guess: String = String::new();
-    let bytes: usize = io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line"); 
-    println!("You guessed: {}, and entered {bytes} bytes.{i}", guess);
+    let secret_number: i32 = rand::thread_rng()
+        .gen_range(1..=100);
+    println!("The secret number is {secret_number}");
+    
+    loop{
+        let mut guess: String = String::new();
+        let bytes: usize = io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line"); 
+        println!("You guessed: {}, and entered {bytes} bytes.", guess.trim());
+        let guess: i32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        };
+    }
 }
