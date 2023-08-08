ff488b7c4969d4018ba376818d5263c3b71a2b1d
felixweiss1999 <felixweiss1999@gmail.com>
2023-08-08 20:50:11 +0200

    included rand crate

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ use std::io;
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess!");
-    let mut guess = String::new();
+    let i: i32 = 23;
+    let mut guess: String = String::new();
     let bytes: usize = io::stdin()
         .read_line(&mut guess)
-            .expect("Failed to read line");
-    println!("You guessed: {}, and entered {bytes} bytes.", guess);
+        .expect("Failed to read line"); 
+    println!("You guessed: {}, and entered {bytes} bytes.{i}", guess);
 }
