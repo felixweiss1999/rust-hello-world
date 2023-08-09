3a2da5d3ed3ffa88af00c887d46196ff683c5538
felixweiss1999 <felixweiss1999@gmail.com>
2023-08-10 00:14:17 +0200

    learned about array bounds checks

--- src/chapter03.rs
+++ src/chapter03.rs
@@ -1,12 +1,17 @@
+
+
 fn main() {
-    let x = 5;
+    let x: i32 = 2147483635;
 
-    let x = x + 1;
+    let x = x + 10;
 
-    {
-        //
-        println!("The value of x in the inner scope is: {x}");
-    }
 
-    println!("The value of x is: {x}");
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    println!("{}", tup.0);
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+    println!("{}", months[1]);
+    let a = [1;200009];
+    println!("{}", a[3000000]);
 }
