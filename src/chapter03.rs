5e3b7549ab5632c030668b9a96ab7ab313e83f8b
felixweiss1999 <felixweiss1999@gmail.com>
2023-08-10 22:48:56 +0200

    checking out conditions

--- src/chapter03.rs
+++ src/chapter03.rs
@@ -1,3 +1,4 @@
+use std::string;
 
 
 fn main() {
@@ -5,14 +6,22 @@ fn main() {
 
     let x = x + 10;
 
-
+    
     let tup: (i32, f64, u8) = (500, 6.4, 1);
     println!("{}", tup.0);
-    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    let _months: [i32; 5] = [1, 2, 3, 4, 5];
     let months = ["January", "February", "March", "April", "May", "June", "July",
               "August", "September", "October", "November", "December"];
     println!("{}", months[1]);
-    let a = [1;200009];
-    println!("{}", a[3000000]);
-    let a: u32 = {};
+    let x: i32 = function(&"hi".to_string());
+    if x == 2 {
+        println!("hihi");
+    } if x != 3 {
+        println!("asdf");
+    }
+}
+
+fn function(x:&String) -> i32{
+    println!("Successfully passed a string: {x}!");
+    2
 }
