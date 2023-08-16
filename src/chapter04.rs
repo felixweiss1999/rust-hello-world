72d6737a6a72d84a4af221dd13700764e711e303
felixweiss1999 <felixweiss1999@gmail.com>
2023-08-16 17:38:17 +0200

    slicing and dicing

--- src/chapter04.rs
+++ src/chapter04.rs
@@ -1,6 +1,21 @@
-fn main() {
-    let s = String::from("hello world");
+fn main(){
+    let s: String = String::from("hello world");
 
-    let hello = &s[0..5];
-    let world = &s[6..11];
+    let hello: &str = &s[0..5];
+    let world: &str = &s[6..11];
+    for i in s.as_str().chars(){
+        println!("{i}");
+    }
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
