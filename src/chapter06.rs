80350115b909b46d1837b9a3ffff682f92d9cfde
felixweiss1999 <felixweiss1999@gmail.com>
2023-08-20 23:12:27 +0200

    enums are actually super useful

--- src/chapter06.rs
+++ src/chapter06.rs
@@ -1,10 +1,20 @@
+#[derive(Debug)]
 enum IpAddrKind {
     V4(u8, u8, u8, u8), 
     V6(String),
 }
 
-fn main() {
-    let ipversion = IpAddrKind::V4;
-    let ipversion6 = IpAddrKind::V6;
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
 
+
+fn main() {
+    let ipversion = IpAddrKind::V4(255, 1, 0, 0);
+    let ipversion6 = IpAddrKind::V6(String::from(""));
+    dbg!(ipversion);
+    dbg!(ipversion6);
 }
