f1fd74f24389662b2ebf677555e53042709a19b7
felixweiss1999 <felixweiss1999@gmail.com>
2023-09-06 13:49:14 +0200

    question mark operator

--- src/chapter09.rs
+++ src/chapter09.rs
@@ -37,3 +37,21 @@ fn read_username_from_file() -> Result<String, io::Error> {
         Err(e) => Err(e),
     }
 }
+
+// equivalent function with ? operators: Key: the ? operator returns the Result(Error) 
+// if error occurs IMMEDIATELY! if not error, it extracts the Ok(value) and execution continues
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut username = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
+}
